9fcb36f0cb1d0107d774a99e45fc63311313a3a8
joshy <joshy-Aspire-5749>
2014-01-05 20:54:52 +0000

    Readme has been added and Greedo changes

--- README.md
+++ README.md
@@ -1,4 +1,80 @@
-eneh-wp-theme
+Eneh-wp-theme
 =============
 
-Eneh Wordpress theme
+Eneh Wordpress themes - Eneh art
+================================
+
+Grid system - Greedo
+====================
+
+Greedo - 12 columns content first SCSS based grid system with 4 breakpoints:
+
+- Death Star for larger screens (big displays)
+- Star destroyer for large screens (laptops, desktops, large tablets)
+- AT-AT for smaller screens (tablets portraits and tablet/mobile landscapes)
+- Tie fighter for small screens (mobile portraits)
+
+Grido follows the content based breakpoint idea which means you have to set up your breakpoints based on your content needs.
+You can modify the settings under the /* Breakpoints */ section and also under /* Grid settings  */ sections in settings.scss.
+
+EMS (em) is the main unit used by Greedo. 
+The formula is target ÷ context = result,
+body font-size: 100% (16px) then 1em = 16px.
+Usage: 21px / 16px = 1.3125em.
+
+http://bradfrostweb.com/blog/post/7-habits-of-highly-effective-media-queries/
+
+CSS3 - SASS
+===========
+
+Used libraries:
+
+- Greedo
+- Normalize
+- Compass support
+- Compass css3
+
+Modules:
+
+- Base
+- Font
+- Layout
+- Menu
+- Page
+- Gallery
+
+Responsive images
+=================
+- responsive-images.js https://github.com/kvendrik/responsive-images.js
+- functions.php add_image_size():
+  - thumbnail: 520x250
+  - smallest: 50x
+  - smaller: 150x
+  - small: 250x
+  - med: 500x
+  - large: 900x
+  - larger: 1000x
+
+Grunt
+=====
+
+- cleaning dist
+- usemin and prepare: concat, uglify, replace
+- cssmin
+- copy to dist
+
+Required WP plugins
+===================
+
+- Advanced custom fields
+- Custom post type UI
+
+Templates
+=========
+
+TODO
+====
+
+- Greedo is content first but not mobile first yet
+- Identation issues (2 spaces should be applied everywhere)
+- Admin functions
--- eneh-art/js/scripts.js
+++ eneh-art/js/scripts.js
@@ -4,87 +4,87 @@ ENEH.site = (function ($, doc, win, eneh) {
 
   parallax = function() {
     $('section[data-type="background"]').each(function(){
-        var $bgobj = $(this);
-        $(win).scroll(function() {
-            var yPos = -($(win).scrollTop() / $bgobj.data('speed')); 
-            // Put together our final background position
-            var coords = '50% '+ yPos + 'px';
-            // Move the background
-            $bgobj.css({ backgroundPosition: coords });
-        }); 
+      var $bgobj = $(this);
+      $(win).scroll(function() {
+        var yPos = -($(win).scrollTop() / $bgobj.data('speed')); 
+        // Put together our final background position
+        var coords = '50% '+ yPos + 'px';
+        // Move the background
+        $bgobj.css({ backgroundPosition: coords });
+      }); 
     }); 
   }
-  
+
   jumpTo = function() {
     $('.jumpto').on('click', function() {
       var dest = $(this).data('destination');
       $('html, body').animate({
-	scrollTop: dest
+        scrollTop: dest
       }, 600);
     });
   }
-  
+
   menuChange = function() {
-  
+
     var scrolled = false;
     //console.log( win.location.pathname);
-    
+
     if(win.location.pathname === '/') {
-  
+
       $(win).scroll(function() {
 
-	if(this.scrollY >= 800 && scrolled === false) {
-	
-	  $("#artist-name").animate({
-	    opacity: 0.55,
-	  }, 10, function() {
-	    $('#artist-icon').parent().removeClass('hide');
-	    $('footer').show(1000);
-	    scrolled = true;
-	  });
-	
-	} else if(scrolled === true && this.scrollY <= 500) {
-	
-	  $("#artist-name").animate({
-	    opacity: 0.55,
-	  }, 10, function() {
-	    $('#artist-icon').parent().addClass('hide');
-	    $('footer').hide();
-	    scrolled = false;
-	  });
-	  
-	}
+        if(this.scrollY >= 800 && scrolled === false) {
+
+          $("#artist-name").animate({
+            opacity: 0.55,
+          }, 10, function() {
+            $('#artist-icon').parent().removeClass('hide');
+            $('footer').show(1000);
+            scrolled = true;
+          });
+
+        } else if(scrolled === true && this.scrollY <= 500) {
+
+          $("#artist-name").animate({
+            opacity: 0.55,
+          }, 10, function() {
+            $('#artist-icon').parent().addClass('hide');
+            $('footer').hide();
+            scrolled = false;
+          });
+
+        }
       });
     } else if(win.location.pathname.indexOf('category') === -1) {
       $('footer').show(1000);
     }
-    
+
   }
-  
+
   bouncingImages = function() {
     $('#art-categories').find('img').on('mouseover', function() {
       $(this).animate({
-	top: "+=25",
+        top: "+=25",
       }, 500)
     }).on('mouseleave', function() {
       $(this).animate({
-	top: "0",
+        top: "0",
       }, 500)
     });
   }
-  
+
   listeners = function() {
     jumpTo();
     menuChange();
     bouncingImages();
-    
+
     $('.hamburger').on('click', function() {
       $('#menu-main').toggle(500);
       $('.bottom-control').toggle(500);
     });
-    
+
   }
-	  
+
   return {
     parallax: parallax,
     listeners: listeners
@@ -93,68 +93,68 @@ ENEH.site = (function ($, doc, win, eneh) {
 }(jQuery, document, window, ENEH));
 
 ENEH.enehgal = (function ($, doc, win, eneh) {
-	
+
   carousel = function() {
     var slides = $('#eneh-slide .art-slide');
     var items  = $(slides).length;
     var width  = $(slides).width();
     var total  = (items*width) + (30*items);
     //$('#eneh-slide').width(total);
-    
+
     window.onload = function() {
-      
+
       $('.image-container').on('click', function() {
-	if($('#menu-main').css('display') === 'none') {
-	  $('.bottom-control').toggle();
-	  $('.top-bar').toggle();
-	  $('#menu-main').hide(500);
-	}
+        if($('#menu-main').css('display') === 'none') {
+          $('.bottom-control').toggle();
+          $('.top-bar').toggle();
+          $('#menu-main').hide(500);
+        }
       });
-      
+
       var slide  = $('#eneh-slide');
-   
+
       $(slide).animate({
-	  left: -200.5
+        left: -200.5
       }, 500, 'linear');
-    
+
       $('#step-left').on('click', function() {
-	$(slide).animate({
-	  left: '-=200.5'
-	}, 500, 'linear', function() {
-	  $('#eneh-slide li:last').after($('#eneh-slide li:first'));
-	  $(slide).css({'left' : -200.5});
-	});
+        $(slide).animate({
+          left: '-=200.5'
+        }, 500, 'linear', function() {
+          $('#eneh-slide li:last').after($('#eneh-slide li:first'));
+          $(slide).css({'left' : -200.5});
+        });
       });
-      
+
       $('#step-right').on('click', function() {
-	$(slide).animate({
-	  left: '+=200.5'
-	}, 500, 'linear', function() {
-	  $('#eneh-slide li:first').before($('#eneh-slide li:last'));
-	  $(slide).css({'left' : -200.5});
-	});
+        $(slide).animate({
+          left: '+=200.5'
+        }, 500, 'linear', function() {
+          $('#eneh-slide li:first').before($('#eneh-slide li:last'));
+          $(slide).css({'left' : -200.5});
+        });
       });
-      
+
       $('.art-slide').on('click', function() {
-	var index = $(this).data('index');
-	var stage = $('.stage-image');
-	$(stage).addClass('hide');
-	$(stage).removeClass('stage-image');
-	var category  = document.getElementById('category'); 
-	var nextstage = category.children[index];
-	$(nextstage).addClass('stage-image').removeClass('hide');
+        var index = $(this).data('index');
+        var stage = $('.stage-image');
+        $(stage).addClass('hide');
+        $(stage).removeClass('stage-image');
+        var category  = document.getElementById('category'); 
+        var nextstage = category.children[index];
+        $(nextstage).addClass('stage-image').removeClass('hide');
       });
-      
+
       $('.image-container').on('mouseover', function() {
-	$('.bottom-control').hide(500);
-	$('.top-bar').hide(500);
+        $('.bottom-control').hide(500);
+        $('.top-bar').hide(500);
       }).on('mouseleave', function() {
-	$('.bottom-control').show(500);
-	$('.top-bar').show(500);
+        $('.bottom-control').show(500);
+        $('.top-bar').show(500);
       });
     }
   }
-  
+
   return {carousel: carousel};
 
 }(jQuery, document, window, ENEH))
@@ -162,8 +162,8 @@ ENEH.enehgal = (function ($, doc, win, eneh) {
 //Misc functions
 function namespace(namespaceString) {
   var parts = namespaceString.split('.'),
-    parent = window,
-    currentPart = '';
+      parent = window,
+      currentPart = '';
 
   for(var i = 0, length = parts.length; i < length; i++) {
     currentPart = parts[i];
